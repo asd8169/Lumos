820617061b9805dbc35c4004ed5755d109498720
ParkSeMi <qkrtpa12@naver.com>
2021-01-23 01:08:59 +0900

    21.01.23 세미 커밋. Admin Product List 띄우기 완료.

---
+++ Lumos/src/main/java/com/spring/camping/admin/product/list/AdminProductList_IDao.java
@@ -0,0 +1,7 @@
+package com.spring.camping.admin.product.list;
+
+import java.util.ArrayList;
+
+public interface AdminProductList_IDao {
+	public ArrayList<AdminProductlistDto> productlistDao();
+}
